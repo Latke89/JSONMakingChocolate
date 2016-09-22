9e29b4d4c3988ea9d52580b4499edd1bff5cb7bd
Brett Gordon <gordonbrett89@gmail.com>
2016-09-22 16:37:28 -0400

    Successfuly created chocolate

--- src/main/resources/static/js/ng-chocolate-controller.js
+++ src/main/resources/static/js/ng-chocolate-controller.js
@@ -9,18 +9,35 @@ angular.module('ChocolateApp', [])
             return returnArray;
         }
 
-        $scope.testTimeout = function() {
-            console.log("testTimeout() is running");
-            if ($scope.numBigs > 5) {
-                return;
-            } else {
-                $scope.numBigs+=1;
-                $timeout($scope.testTimeout, 1000);
-            }
-        }
+//        $scope.testTimeout = function() {
+//            console.log("testTimeout() is running");
+//            if ($scope.numBigs > 5) {
+//                return;
+//            } else {
+//                $scope.numBigs+=1;
+//                $timeout($scope.testTimeout, 1000);
+//            }
+//        }
+//
+//        $scope.numBigs = 1;
+//        $timeout($scope.testTimeout, 1000);
 
-        $scope.numBigs = 1;
-        $timeout($scope.testTimeout, 1000);
+           $scope.solveChocolate = function(numSmalls, numBigs, numGoal) {
+               console.log("About to go make chocolate!");
+
+               $http.get("http://localhost:8080/chocolate.json?small=" + numSmalls + "&big=" + numBigs + "&goal=" + numGoal)
+                   .then(
+                       function successCallback(response) {
+                           console.log(response.data);
+                           console.log("Adding data to scope");
+//                           $scope.todos = response.data;
+                       },
+                       function errorCallback(response) {
+                           console.log("Unable to get data");
+                       });
+               console.log("Done with the promise, waiting for the callback")
+           };
+
 
         console.log("chocolate controller initialized");
         $scope.testvar = "blargh";
